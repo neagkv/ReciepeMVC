bd2268bdc7b8c3c6f21fcddd2ce940ab8aea2ffb
Kevin Neag <neagkv@gmail.com>
2018-05-08 06:55:50 -0400

    create new recipe

--- src/main/java/com/reciepe/controllers/RecipeController.java
+++ src/main/java/com/reciepe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.reciepe.controllers;
 
+import com.reciepe.command.RecipeCommand;
 import com.reciepe.domain.Recipe;
 import com.reciepe.service.RecipeService;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -26,6 +29,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
 
