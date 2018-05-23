e6bd9fbafeb62d769c31f1b7a5213255752a31ee
Kevin Neag <neagkv@gmail.com>
2018-05-23 17:29:35 -0400

    upload image from h2

--- src/test/java/com/reciepe/controllers/ImageControllerTest.java
+++ src/test/java/com/reciepe/controllers/ImageControllerTest.java
@@ -7,9 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
@@ -72,5 +75,35 @@ public class ImageControllerTest {
                     .andExpect(header().string("Location", "/recipe/1/show"));
 
             verify(imageService, times(1)).saveImageFile(anyLong(), any());
+        }
+
+        @Test
+        public void renderImageFromDB() throws Exception {
+
+            //given
+            RecipeCommand recipeCommand = new RecipeCommand();
+            recipeCommand.setId(1L);
+
+            String s = "fake image text";
+            Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+            int i = 0;
+
+            for (byte primByte : s.getBytes()){
+                bytesBoxed[i++] = primByte;
+            }
+
+            recipeCommand.setImage(bytesBoxed);
+
+            when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+            //when
+            MockHttpServletResponse response = mockMvc.perform(get("recipe/1/recipeimage"))
+                    .andExpect(status().isOk())
+                    .andReturn().getResponse();
+
+            byte[]responseBytes = response.getContentAsByteArray();
+
+            assertEquals(s.getBytes().length, responseBytes.length);
         }
 }
