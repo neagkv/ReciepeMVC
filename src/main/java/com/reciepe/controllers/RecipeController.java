2422dfb8158c7cfe81d983e641f8df15aa368050
Kevin Neag <neagkv@gmail.com>
2018-05-27 19:42:19 -0400

    added validation to reciepeCommand

--- src/main/java/com/reciepe/controllers/RecipeController.java
+++ src/main/java/com/reciepe/controllers/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * @author Kevin Neag
@@ -18,6 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RecipeController {
 
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
@@ -32,32 +36,42 @@ public class RecipeController {
         return "recipe/show";
     }
 
-    @GetMapping("/recipe/new")
+    @GetMapping("recipe/new")
     public String newRecipe(Model model){
-        model.addAttribute("recipe",new RecipeCommand());
+        model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
 
+    @GetMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return RECIPE_RECIPEFORM_URL;
+    }
+
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @GetMapping("recipe/{id}/update")
-    public String updateRecipe(@PathVariable String id, Model model){
-        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
-    }
-
     @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
+
         log.debug("Deleting id: " + id);
 
         recipeService.deleteById(Long.valueOf(id));
-
         return "redirect:/";
     }
 
@@ -71,12 +85,10 @@ public class RecipeController {
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("404error");
-
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
-
 
 }
 
--- src/test/java/com/reciepe/controllers/RecipeContorllerTest.java
+++ src/test/java/com/reciepe/controllers/RecipeContorllerTest.java
@@ -41,7 +41,20 @@ public class RecipeContorllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setControllerAdvice(new ControllerExceptionHandler())
                 .build();
+    }
 
+    @Test
+    public void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 
     @Test
@@ -57,23 +70,9 @@ public class RecipeContorllerTest {
     @Test
     public void testGetRecipeNumberFormatException() throws Exception {
 
-        mockMvc.perform(get("/recipe/hhghjh/show"))
+        mockMvc.perform(get("/recipe/asdf/show"))
                 .andExpect(status().isBadRequest())
                 .andExpect(view().name("400error"));
-    }
-
-    @Test
-    public void testGetRecipe() throws Exception {
-
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
-        when(recipeService.findById(anyLong())).thenReturn(recipe);
-
-        mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"))
-                .andExpect(model().attributeExists("recipe"));
     }
 
     @Test
@@ -97,9 +96,27 @@ public class RecipeContorllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
